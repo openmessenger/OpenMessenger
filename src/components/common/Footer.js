53279269e4049657e62bb0a315c2472c45d93e6d
psjishnu <psjishnu13@gmail.com>
2020-07-31 23:30:35 +0530

    Updates A

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { A } from "hookrouter";
 
 const Footer = () => {
     return (
         <div className="bottom-0 w-full">
             <section className="py-3 w-full">
                 <div className="container text-center mx-auto px-8">
-                    <A
+                    <a
                         href="http://github.com/openmessenger"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="text-black text-center font-bold text-sm sm:mb-2 hover:text-green-700 ">
                         Contribute on Github{" "}
                         <i className="fa fa-github text-xl"></i>
-                    </A>
+                    </a>
                 </div>
             </section>
         </div>
